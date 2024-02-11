25e8bb5ce1351664e164ab5f7e9ef7d21af01ee6
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-02-11 02:34:53 -0800

    Printf Task 0, try 8.

--- 2-strlen.c
+++ 2-strlen.c
@@ -16,3 +16,20 @@ int _strlen(char *s)
 
 	return (length);
 }
+
+/**
+ * _strlen - Determines the length of a string.
+ * @s: String to pass in function.
+ *
+ * Return: Length of string.
+ */
+int _strlenc(const char *s)
+{
+	int length;
+
+	length = 0;
+	while (*(s + length) != '\0')
+		length++;
+
+	return (length);
+}
