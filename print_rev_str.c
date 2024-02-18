00a07699ee2ca0a779d142872af62c3180fdf92c
Jean-Paul KYOKYA <kalkyokya4@gmail.com>
2024-02-18 13:50:47 -0800

    Printf Task 0, try 28.

--- print_rev_str.c
+++ print_rev_str.c
@@ -11,15 +11,14 @@ int print_rev_str(va_list ptr_to_arg)
 	char *s;
 	int size, len;
 
-	len = 0;
 	s = va_arg(ptr_to_arg, const char *);
 	size = _strlenc(s);
-	while (size >= 0)
+	len = size - 1;
+	while (len >= 0)
 	{
-		size--;
 		_putchar(s[size]);
-		len++;
+		len--;
 	}
 
-	return (len);
+	return (size);
 }
